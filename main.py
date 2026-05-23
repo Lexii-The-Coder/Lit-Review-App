824ea55b09d16a24d6f58032c4c1c0501cbfce90
Teepeelex <alex.weirauch@dri.edu>
2026-05-22 23:48:51 -0700

    further changed dependencies

--- main.py
+++ main.py
@@ -336,7 +336,7 @@ with tab2:
                 labels={'x': 'Research Area', 'y': 'Number of Papers'},
                 title='Papers by Research Area'
             )
-            st.plotly_chart(fig1, width=True)
+            st.plotly_chart(fig1)
         
         with col2:
             fig2 = px.histogram(
@@ -346,7 +346,7 @@ with tab2:
                 labels={'relevance_score': 'Relevance Score'},
                 title='Relevance Score Distribution'
             )
-            st.plotly_chart(fig2, width=True)
+            st.plotly_chart(fig2)
         
         col1, col2 = st.columns(2)
         
@@ -356,7 +356,7 @@ with tab2:
                 y='word_count',
                 title='Word Count Distribution'
             )
-            st.plotly_chart(fig3, width=True)
+            st.plotly_chart(fig3)
         
         with col2:
             fig4 = px.scatter(
@@ -368,7 +368,7 @@ with tab2:
                 labels={'references': 'Number of References', 'relevance_score': 'Relevance Score'},
                 title='References vs Relevance'
             )
-            st.plotly_chart(fig4, width=True)
+            st.plotly_chart(fig4)
 
 # TAB 3: Paper Library
 with tab3:
