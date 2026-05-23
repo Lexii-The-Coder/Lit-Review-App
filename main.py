39d0ee1b917ee738311aa20fc44b1b22b9870459
Teepeelex <alex.weirauch@dri.edu>
2026-05-23 00:32:28 -0700

    import update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
 import json
+import re
 from openai import OpenAI
 from difflib import SequenceMatcher
 from pyvis.network import Network
